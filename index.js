95e1d7a4b1b53c3751e1ca7c22c09703e71f59ca
Marli <marleysumarly@gmail.com>
2023-08-09 17:27:31 +0700

    Update
    Description

--- index.js
+++ index.js
@@ -5,22 +5,21 @@ import Octokit from "@octokit/core";
 
 function main() {
 
-    console.log(Github.context.payload.commits);
 
-    // try {
-    //     const notion = new NotionService(secret, database);
-    //     notion.addItem({
-    //         commitTittle: "Asd",
-    //         commitDescription: "Dsa",
-    //         commitBy: "Marleess",
-    //         commitUrl: "https://www.postman.com/notionhq/workspace/notion-s-api-workspace/request/15568543-a027e2d4-11f2-4068-a82c-cb9fb8562036",
-    //         project: "coba-project",
-    //         commitId: "123"
-    //     });
-    // } catch (error) {
-    //
-    // }
+    try {
+        console.log(Github.context.payload.commits);
+        // const notion = new NotionService(secret, database);
+        // notion.addItem({
+        //     commitTittle: "Asd",
+        //     commitDescription: "Dsa",
+        //     commitBy: "Marleess",
+        //     commitUrl: "https://www.postman.com/notionhq/workspace/notion-s-api-workspace/request/15568543-a027e2d4-11f2-4068-a82c-cb9fb8562036",
+        //     project: "coba-project",
+        //     commitId: "123"
+        // });
+    } catch (error) {
+
+    }
 }
 
 main();
-console.log("Test");
