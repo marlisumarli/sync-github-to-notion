d91a7505059460a5f138db97e359cea014a4acc3
Marli <marleysumarly@gmail.com>
2023-08-09 16:54:20 +0700

    Add workflows

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 import {NotionService} from "./src/services/notion.service.js";
-import 'dotenv/config'
-// core GitHub
+import Core from "@actions/core";
+import Github from "@actions/github";
+import Octokit from "@octokit/core";
 
-const secret = process.env.NOTION_SECRET;
-const database = process.env.DATABASE_ID;
+function main() {
 
-const notion = new NotionService(secret, database);
-notion.addItem({
-    commitTittle: "Asd",
-    commitDescription: "Dsa",
-    commitBy: "Marleess",
-    commitUrl: "https://www.postman.com/notionhq/workspace/notion-s-api-workspace/request/15568543-a027e2d4-11f2-4068-a82c-cb9fb8562036",
-    project: "coba-project",
-    commitId: "123"
-});
+    const notion = NotionService
+    const core = Core
+    const github = Github
+    const octokit = Octokit
+
+    // try {
+    //     const notion = new NotionService(secret, database);
+    //     notion.addItem({
+    //         commitTittle: "Asd",
+    //         commitDescription: "Dsa",
+    //         commitBy: "Marleess",
+    //         commitUrl: "https://www.postman.com/notionhq/workspace/notion-s-api-workspace/request/15568543-a027e2d4-11f2-4068-a82c-cb9fb8562036",
+    //         project: "coba-project",
+    //         commitId: "123"
+    //     });
+    // } catch (error) {
+    //
+    // }
+}
+
+console.log("running");
