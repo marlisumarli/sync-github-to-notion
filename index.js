fd3a9f2ed73d0fe67c3185342bb6480074f27cc5
Marli <marleysumarly@gmail.com>
2023-08-29 21:07:17 +0700

    Commit

--- index.js
+++ index.js
@@ -9,25 +9,28 @@ function main() {
         const secret = Core.getInput('notion_secret');
         const database = Core.getInput('notion_database');
 
-        commits.forEach((commit) => {
-            const array = commit.message.split(/\r?\n/);
-            const title = array.shift();
+        // commits.forEach((commit) => {
+        //     const array = commit.message.split(/\r?\n/);
+        //     const title = array.shift();
+        //
+        //     let description = "";
+        //     array.forEach((element) => {
+        //         description += " " + element;
+        //     });
+        //
+        //     const notion = new NotionApi(secret, database);
+        //     notion.addItem({
+        //         commitTittle: title,
+        //         commitDescription: description,
+        //         commitBy: commit.author.name,
+        //         commitUrl: commit.url,
+        //         project: Github.context.repo.repo,
+        //         commitId: commit.id
+        //     });
+        // });
 
-            let description = "";
-            array.forEach((element) => {
-                description += " " + element;
-            });
-
-            const notion = new NotionApi(secret, database);
-            notion.addItem({
-                commitTittle: title,
-                commitDescription: description,
-                commitBy: commit.author.name,
-                commitUrl: commit.url,
-                project: Github.context.repo.repo,
-                commitId: commit.id
-            });
-        });
+        console.log(Github.context.payload);
+        console.log(commits);
     } catch (error) {
         console.error(error);
     }
