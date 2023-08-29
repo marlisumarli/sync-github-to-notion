7a7b7530a494fa13d8838ac867fa7f59a00d6ce0
Marli <marleysumarly@gmail.com>
2023-08-30 00:02:55 +0700

    Commit

--- app/notion-api.js
+++ app/notion-api.js
@@ -77,7 +77,7 @@ export class NotionApi {
                 },
             });
         } catch (error) {
-            Core.setFailed(`Error: ${error}`);
+            Core.setFailed(`Error: ${error.message.message}`);
         }
     }
 }
