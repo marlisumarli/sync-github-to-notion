0f047343a1da250429518c5be406ccec3719b9f9
Marli <marleysumarly@gmail.com>
2023-08-29 22:44:05 +0700

    Commit

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function main() {
                 commitId: commit.id
             });
         });
-        console.log(Github.context.payload.commits);
+        console.log(Github.context.ref);
     } catch (error) {
         Core.setFailed(`Error: ${error.message}`);
     }
